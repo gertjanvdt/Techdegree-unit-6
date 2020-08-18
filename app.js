358fc62282500f59f3186840f33484df7a8a5d4f
gertjanvdt <64344880+gertjanvdt@users.noreply.github.com>
2020-08-18 13:40:39 +0800

    expanded functions on the game

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-// DECLARED VARIABLES
+// ***** DECLARED VARIABLES *****
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const letterButton = document.querySelectorAll('button');
 let missed = 0;
 
 const phrases = [
-    "The Lion King",
-    "Beauty and the Beast",
-    "The Little Mermaid",
-    "The Jungle Book",
-    "Lady and the Tramp"
+    "the lion king",
+    "beauty and the beast",
+    "the little mermaid",
+    "the jungle book",
+    "lady and the tramp"
 ]
 
-// START GAME
-const overlay = document.getElementById('overlay');
-overlay.addEventListener('click', (e) => {
-    
-    if (event.target.className === 'btn__reset') {
-        overlay.style.display = 'none';
-    }
-});
-
-// FUNCTIONS
+// ***** FUNCTIONS *****
 
 // Step 5
 function getRandomPhraseAsArray(arr) {
@@ -59,35 +51,85 @@ function addPhraseToDisplay (arr) {
     }
 }
 
-addPhraseToDisplay(pickedPhraseArray);
-
-// Step 7
+// Step 7 - Function to check if guessed letter is present in sentence
 function checkLetter(letter) {
     // Get all li elements with class: letter
     const liLetters = document.getElementsByClassName('letter');
+    // Other variables used in function 
+    let letterFound = null;
     
     // Loop through all li letters and check if match with pressed letter
     for (let i = 0; i < liLetters.length; i++) {
-        if (liLetters.textContent[i] === letter) {
-            liLetters.className[i] = 'show';
-            const guessedLetter = liLetters[i];
-            return guessedLetter;
-        } else {
-            return null;
+        if (liLetters[i].textContent === letter) {
+            liLetters[i].className += ' ' + 'show';
+            letterFound = liLetters[i].textContent;
         }
    } 
+   // If the letter is guessed return the letter or otherwise null
+   return letterFound;
 }
 
-// Step 8
-qwerty.addEventListener('click', (e) => {
-    if (event.target.tagName === 'button') {
-        const letter = event.target;
-        letter.className = 'chosen';
-        checkLetter(letter.textContent);
-        console.log('letter clicked')
-        
-    } else {console.log('if not working');}
+// Step 10 - Function to check if player wins or loses upon clicking the letter
+function checkWin () {
+    const showLetters = document.getElementsByClassName('show');
+    const letters = document.getElementsByClassName('letter');
+    const totalShownLetters = showLetters.length;
+    const totalLetters = letters.length;
+
+    if (missed >= 5) {
+        winOrLose('lose');
+    } else if (totalShownLetters === totalLetters) {
+        winOrLose('win');
+    }   
+}
+
+// Step 10 - Function for action on win or lose result
+function winOrLose (result) {
+    const overlayTitle = document.querySelector('.title');
+    const startButton = document.querySelector('.btn__reset');
+
+    overlay.style.display = '';
+    overlay.className = result;
+    overlayTitle.innerHTML = `YOU ${result} THE GAME`;
+    startButton.innerHTML = 'Start New Game'
+}
+
+
+// *****  START GAME  *****
+
+const overlay = document.getElementById('overlay');
+overlay.addEventListener('click', (e) => {
+    
+    if (event.target.className === 'btn__reset') {
+        overlay.style.display = 'none';
+    }
 });
+
+addPhraseToDisplay(pickedPhraseArray);
+
+// Step 8, litlen for letter click and 
+qwerty.addEventListener('click', (e) => {  
+    if (event.target = letterButton) {
+    const letter = event.target;
+    letter.className = 'chosen';
+    
+    const letterFound = checkLetter(letter.textContent)
+  
+    // Step 9 
+    // Increase missed variable if letter not result
+        if (letterFound === null) {
+            missed++
+        }
+    }
+    // Step 10
+    checkWin();
+});
+
+
+
+
+
+
 
 
 
